7439387001b9e5c90bae77f439c3e787f920c7ee
Oleg Vargin <oleg_197@bk.ru>
2021-04-10 13:51:56 +0300

    Hid status bar

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -8,14 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var isLightOn = false
+    
+    var isLightOn = true
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
-
+    
     fileprivate func updateUI() {
         view.backgroundColor = isLightOn ? .white : .black
     }
@@ -23,7 +27,7 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed() {
         isLightOn.toggle()
         updateUI()
-           
+        
     }
 }
 
